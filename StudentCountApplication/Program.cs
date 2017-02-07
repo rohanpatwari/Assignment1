803bc7d2c361e897f7259a91cf63ac8d84ea99a5
rohan <rohan@DESKTOP-2911EVS>
2017-02-06 19:38:33 -0600

    Add project files.

---
+++ StudentCountApplication/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace StudentCountApplication
+{
+    public class Student
+    {
+        public string FName;
+        public string LName;
+        public static int num;
+        public Student()
+        {
+            num++;
+        }
+        public static int GetNum()
+        {
+            return num;
+        }
+        public void PrintFullName(string FN, string LN)
+        {
+            string FullName;
+            FName = FN;
+            LName = LN;
+            FullName = FName + " " + LName;
+            Console.WriteLine("FullName: {0}", FullName);    
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Student s1 = new Student();
+            s.PrintFullName("Rohan","Patwari");
+             s1.PrintFullName("Rishi", "Patwari");
+            Console.WriteLine("Total No Of Students: {0}", Student.GetNum());
+            Console.ReadLine();
+         
+        }
+    }
+}
